b3c2845de8a6c2cca7dd5c3949e59c0ef7e84aa9
Jason Watson <jswatson0@gmail.com>
2014-06-25 14:46:29 -0500

    Add documentation

--- rotational_cipher.rb
+++ rotational_cipher.rb
@@ -1,60 +1,53 @@
 class RotationalCipher
-
-  attr_accessor :numeric_message, :numeric_encoded_message, :encoded_message, :decoded_message
+  attr_accessor :numeric_message, :numeric_encoded_message, :encoded_message
 
   ALPHA = ("a".."z").to_a
   ALPHA_HASH = Hash[ALPHA.map.with_index.to_a]
 
+  # Creates encoded message when passed integer and string
   def rotx(x, string, encrypt=true)
     numberfy(string)
-    apply_cypher(x)
-
-    # encode_message
-    # self.encoded_message.join
+    apply_cipher(x)
 
     if encrypt == true
       encode_message
       self.encoded_message.join
     else
-      @decoded_message = []
-      @numeric_message.each do |num|
-        new_letter = ALPHA_HASH.invert[num]
-        @decoded_message << new_letter
-      end
-      self.decoded_message.join
+      # TODO add decryption ability
+      # self.decoded_message
     end
   end
 
+  # Assigns numeric value to each letter of string and assembles as an Array
   def numberfy(string)
-    message = string.split(//)
     @numeric_message = []
-
+    message = string.split(//)
     message.each do |letter|
       number = ALPHA_HASH[letter]
       @numeric_message << number
     end
   end
 
-  def apply_cypher(x)
+  # Creates numeric encoded message based on cipher integer
+  def apply_cipher(x)
     @numeric_encoded_message = []
-
     @numeric_message.each do |num|
       new_num = num + x
       @numeric_encoded_message << new_num
     end
 
+    # ensures encoded number falls within correct range
     @numeric_encoded_message.collect! do |num|
-        num > 26 ? (num - 26) : num
+      num > 26 ? (num - 26) : num
     end
   end
 
+  # Replaces number value with corresponding letter value
   def encode_message
     @encoded_message = []
-
     @numeric_encoded_message.each do |num|
       new_letter = ALPHA_HASH.invert[num]
       @encoded_message << new_letter
     end
-    @encoded_message.join
   end
 end
