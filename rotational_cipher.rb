22b5ba9a2679eccd4985b2173212d7c8b8df7e63
Jason Watson <jswatson0@gmail.com>
2014-07-01 19:30:58 -0500

    feature complete (basicaly)

--- rotational_cipher.rb
+++ rotational_cipher.rb
@@ -1,5 +1,6 @@
 class RotationalCipher
-  attr_accessor :numeric_message, :numeric_encoded_message, :encoded_message, :decoded_message, :numeric_decoded_message
+  attr_accessor :numeric_message, :numeric_encoded_message, :encoded_message, :decoded_message,
+                :numeric_decoded_message
 
   ALPHA = ("a".."z").to_a
   ALPHA_HASH = Hash[ALPHA.map.with_index.to_a]
@@ -19,57 +20,60 @@ class RotationalCipher
     if encrypt == true
       apply_cipher(x)
       encode_message
-      self.encoded_message.join
     else
-      # TODO add decryption ability
       reverse_cipher(x)
       decode_message
-      self.decoded_message.join
     end
   end
 
   # Assigns numeric value to each letter of string and assembles as an Array
   def numberfy(string)
-    message = string.split(//)
-    message.each do |letter|
-      number = ALPHA_HASH[letter]
+    message = string.downcase.split(//)
+    message.each do |char|
+      (ALPHA.include? char) ? number = ALPHA_HASH[char] : number = char
       @numeric_message << number
     end
   end
 
   # Creates encoded numeric message based on cipher integer
   def apply_cipher(x)
-    @numeric_message.each do |num|
-      num += x
-      num -= 26 while num >= 26
-      @numeric_encoded_message << num
+    @numeric_message.each do |char|
+      if char.class == Fixnum
+        char += x
+        char -= 26 while char >= 26
+      else
+        char
+      end
+      @numeric_encoded_message << char
     end
   end
 
   def reverse_cipher(x)
-    @numeric_message.each do |num|
-      num -= x
-      if num > 26
-        num -= 26 while num >= 26
+    @numeric_message.each do |char|
+      if char.class == Fixnum
+        char -= x
+        (char > 26) ? (char -= 26 while char >= 26) : char += 26 while char < 0
       else
-        num += 26 while num < 0
+        char
       end
-      @numeric_decoded_message << num
+      @numeric_decoded_message << char
     end
   end
 
   # Replaces number value with corresponding letter value
   def encode_message
-    @numeric_encoded_message.each do |num|
-      new_letter = ALPHA_HASH.invert[num]
-      @encoded_message << new_letter
-    end
+    self.convert(@numeric_encoded_message, @encoded_message)
   end
 
   def decode_message
-    @numeric_decoded_message.each do |num|
-      new_letter = ALPHA_HASH.invert[num]
-      @decoded_message << new_letter
+    self.convert(@numeric_decoded_message, @decoded_message)
+  end
+
+  def convert(numeric_message, alpha_message)
+    numeric_message.each do |char|
+      (char.class == Fixnum) ? new_letter = ALPHA_HASH.invert[char] : new_letter = char
+      alpha_message << new_letter
     end
+    alpha_message.join
   end
 end
